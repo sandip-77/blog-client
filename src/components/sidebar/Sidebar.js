4ea58543c97000883ffae5dfd2714775cd7c7dea
Sandip Rout <pmsandip8@gmail.com>
2021-06-04 12:18:52 +0530

    changed get request

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -9,7 +9,7 @@ const Sidebar = () => {
 
     useEffect(() => {
         const getCats = async () => {
-            const res = await axios.get("/categories")
+            const res = await axios.get("https://sandip-blog-api.herokuapp.com/api/categories")
             setCats(res.data);
         }
         getCats();
